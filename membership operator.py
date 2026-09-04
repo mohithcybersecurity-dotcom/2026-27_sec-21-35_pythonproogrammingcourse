96beb071ac6e48ae005c4c6d6649de7e473c322d
mohithcybersecurity-dotcom <mohithcybersecurity@gmail.com>
2026-09-04 14:39:24 +0530

    membership_operator

---
+++ membership operator.py
@@ -0,0 +1,20 @@
+#Membership operators
+a=[1,2,3]
+b=2
+result=b in a #in membership opertor
+print("result of ",b,"in",a,"is:",result)
+
+a=[4,5,6]
+b=2
+result=b not in a #not in membership operator
+print("result of",b,"not in",a,"is:",result)
+
+b=[7,5,0]
+a=6
+result=a in b#in membership operator
+print("result of",a,"in",b,"is:",result)
+
+m='python'
+k='p'
+reult=k not in m #not in membership operator
+print("result of ",k,"not in",m,"is;",result)
