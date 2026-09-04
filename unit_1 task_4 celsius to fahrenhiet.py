8ef9c20e3d51368453f27683c2d5ede8df9bfbb1
mohithcybersecurity-dotcom <mohithcybersecurity@gmail.com>
2026-09-04 14:51:20 +0530

    celsius to fahrenheit conversion

---
+++ unit_1 task_4 celsius to fahrenhiet.py
@@ -0,0 +1,5 @@
+#converting celsius to fahrenheit
+celsius=float(input("Enter temperature in celsius:"))
+fahrenheit=(celsius*9/5)+32
+print("Temperture in fahrenheit is:" ,fahrenheit)   
+print("Temperture in celsius is:",  celsius)                 
