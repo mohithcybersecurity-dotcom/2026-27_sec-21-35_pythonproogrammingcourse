b6b189f582e2d5cf65e3019823cea37b899d0261
Palli Mohith <mohithcybersecurity@gmail.com>
2026-09-07 19:00:48 +0530

    Comparison operator

---
+++ comparison operator.py
@@ -0,0 +1,34 @@
+#comparison operator exercise
+l=4
+v=8
+result=l>v#greater than operator 
+print("result of",l,">",v,"is:",result)
+
+s=98
+d=38
+result=s<d#less than operator 
+print("result of",s,"<",d,"is:",result)
+
+b=93
+x=48
+result=b>=x#greater than or equal to operator
+print("result of",b,">=",x,"is:",result)
+
+n=48
+m=37
+result=n<=m#less than or equal to operator 
+print("result of",n,"<=",m,"is:",result)
+
+c=44
+m=90
+result=c==m#equal to operator
+print("result of",c,"==",m,"is:",result)
+
+v=38
+t=32
+result=v!=t#not equal to operator
+print("result of",v,"!=",t,"is:",result)
+
+
+
+
