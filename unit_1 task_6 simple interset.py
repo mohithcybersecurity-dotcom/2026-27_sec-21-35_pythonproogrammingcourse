8a712b25e6eb1ef84fe50cd3661a353e4ac5d7e3
Palli Mohith <mohithcybersecurity@gmail.com>
2026-09-07 19:01:43 +0530

    simple ineterest

---
+++ unit_1 task_6 simple interset.py
@@ -0,0 +1,7 @@
+#To calculate simple interest
+principal_amount=float(input("Enter the principal amount:"))
+rate_of_interest=float(input("Enter the rate of interest:"))
+time=float(input("enter time in years:"))
+simple_interest=(principal_amount*rate_of_interest*time)/100
+print("The simple interest is :",simple_interest)
+
