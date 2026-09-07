15f3279c29aa32207f0b00052c0a2f695eb97cb6
Palli Mohith <mohithcybersecurity@gmail.com>
2026-09-07 18:19:34 +0530

    Logical  operator

---
+++ logical_operators.py
@@ -0,0 +1,22 @@
+#python logical opearators exercise
+a=7
+result=a>2 and a<28#logical and operator
+print("result of ",a,">2 and",a,"<28:", result)
+
+j=8
+result=j<2 and j>28#logical and operator
+print("result of ",j,",<2 and ",j,">28:", result)
+
+k=2
+result=k>2 or k<67#logical or operator
+print("result of ",k,">2 or ",k,"<67:", result)
+
+result=k<1 or k>68#logical or operator
+print("result of ",k,"<1 or ",k,">68:", result)
+
+n=90
+result=not(n>100)#logical not operator
+print("result of not(",n,">100):", result)
+
+result=not(n<100)#logical not opoerator
+print("result of not(",n,"<100):", result)
