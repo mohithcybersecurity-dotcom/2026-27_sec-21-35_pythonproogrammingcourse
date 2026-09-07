99cf8f6458140fc9c4abb5b83bbc711003606748
Palli Mohith <mohithcybersecurity@gmail.com>
2026-09-07 19:00:17 +0530

    arithematic operator

---
+++ arithematic operator.py
@@ -0,0 +1,36 @@
+#arithematic operator exercise
+a=9
+b=66
+result=a+b#addition operator
+print("result of ",a,"+",b,"is:",result)
+
+g=4
+k=33
+result=g-k#subtraction operator 
+print("result of",g,"-",k,"is:",result)
+
+d=3
+l=4
+result=d*l#multiplication operator 
+print("result of ",d,"*",l,"is:",result)
+
+v=3
+w=9
+result=v/w#division operator
+print("result of ",v,"/",w,"is:",result)
+
+q=93
+z=38
+result=q%z#modulus operator
+print("result of",q,"%",z,"is:",result)
+
+b=45
+l=86
+result=b**l#exponential operator
+print("result of ",b,"**",l,"is:", result)
+
+o=3
+i=87
+result=o//i#floor divison operator
+print("result of ",o,"//",i,"is:", result)
+
