418965b2435412ae9b5b60b71848305122abe631
Palli Mohith <mohithcybersecurity@gmail.com>
2026-09-09 11:30:58 +0530

    Sum of n natural numbers

---
+++ unit_1 task_8 sum_of_n_natural_numbers.py
@@ -0,0 +1,15 @@
+#sum of n natural numbers
+n=int(input("enter a number:"))
+sum=0
+for i in range(n):
+    sum+=i
+print("sum of n natural numbers is:",sum)
+
+n=int(input("enter a number:"))
+sum=0
+for i in range(1,n+1):
+    sum+=i
+print("sum of n natural numbers is:",sum)
+
+
+
